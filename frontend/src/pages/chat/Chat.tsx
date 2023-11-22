8b11dfedb8b13d71d993119074ebe326e760507a
Prosenjit Das <prosenjitd0984@gmail.com>
2023-11-22 14:24:28 -0500

    changed logo

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -575,7 +575,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Azure}
+                                    src={BizPartNER}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
